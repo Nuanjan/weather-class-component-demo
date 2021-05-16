ec366ef3a033593f7f7950b9ace0a60100743c58
Nuanjan <nuanjan.schluntz@gmail.com>
2021-05-16 15:03:13 -0400

    handering return JSX component after state change

--- src/index.js
+++ src/index.js
@@ -6,14 +6,25 @@ class App extends React.Component {
         // referrence of react component to make sure it get call.
         super(props);
 
-        this.state = {lat: null};
+        this.state = {
+            lat: null,
+            errorMsg: ''
+        };
     }
     render() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => {
+                console.log(position.coords)
+                this.setState({lat : position.coords.latitude})
+            },
+            (err) => this.setState({errorMsg: err.message})
         );
-        return <div>Latitude: {this.state.lat}</div>;
+    
+        if(this.state.errorMsg && !this.state.lat) return <div>Error: {this.state.errorMsg}</div>
+        if(!this.state.errorMsg && !this.state.lat) return <div>Loading...</div>         
+        
+         return <div>Latitude: {this.state.lat}</div>
+
     }
 }
 
